1b5c6ff34f3d069e5a62a2500b40dc1d14a5fe67
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-07-02 12:38:16 +0530

    added user authentication

--- src/endpoints/user_management.py
+++ src/endpoints/user_management.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from src import mf_user_management
 from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
+from src.dependency import get_current_user
+
 
 router = APIRouter()
 
@@ -31,19 +34,28 @@ class Login(BaseModel):
 
 @router.post("/add_user")
 async def add_user(Add: AddUser ):
-    value = mf_user_management.add_user(username=Add.user_name,
-                                        email=Add.email,
-                                        password=Add.password,
-                                        first_name=Add.first_name,
-                                        last_name=Add.last_name,
-                                        phone_number=Add.phone_number,
-                                        )
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
-
+    if get_current_user() ==1:
+        value = mf_user_management.add_user(username=Add.user_name,
+                                            email=Add.email,
+                                            password=Add.password,
+                                            first_name=Add.first_name,
+                                            last_name=Add.last_name,
+                                            phone_number=Add.phone_number,
+                                            )
+        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
+    else:
+        return RedirectResponse(url='/login')
+    
+  
 @router.get("/list_user")
 async def list_user():
-    value, data = mf_user_management.list_user()
-    if value ==1:
-        {"status": "SUCCESS", "data": data}
+    if get_current_user() ==1:
+        value, data = mf_user_management.list_user()
+        if value ==1:
+            {"status": "SUCCESS", "data": data}
+        else:
+            {"status": "FAILED", "data": data}
     else:
-        {"status": "FAILED", "data": data}
+        return RedirectResponse(url='/login')
+    
+    
--- src/endpoints/validation.py
+++ src/endpoints/validation.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, File, UploadFile, Form
 import shutil
 import os
 from src.dependency import get_current_user
+from fastapi.responses import RedirectResponse
 
 router = APIRouter(
     dependencies=[Depends(get_current_user)]
@@ -12,26 +13,30 @@ router = APIRouter(
 
 @router.post("/validation")
 async def validation(file: UploadFile = File(...), program_type: str = Form(...), media_type: str = Form(...)):
-    try:
-        file_location = f"temp_files/{file.filename}"
-        # Ensure the directory exists
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+    if get_current_user() == 1:
+        try:
+            file_location = f"temp_files/{file.filename}"
+            # Ensure the directory exists
+            os.makedirs(os.path.dirname(file_location), exist_ok=True)
 
-        # Save the file to the specified location
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-        
-        if media_type =="pdf":
-            value, response = mf_validator.validation(file_location, program_type)
-            # list_2d = [inner for outer in response for inner in outer]
-            os.remove(file_location)
-            return {"status": "SUCCESS" if value == 1 else "FAILED", "data": response}
-        elif media_type == "Video":
-            value, data = mf_validator.transcript(file_location)
-            os.remove(file_location)
-            return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
-        # Delete the file after processing
-        
-    except Exception as e:
-        return {"status": "FAILED", "data": str(e)}
+            # Save the file to the specified location
+            with open(file_location, "wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
+            
+            if media_type =="pdf":
+                value, response = mf_validator.validation(file_location, program_type)
+                # list_2d = [inner for outer in response for inner in outer]
+                os.remove(file_location)
+                return {"status": "SUCCESS" if value == 1 else "FAILED", "data": response}
+            elif media_type == "Video":
+                value, data = mf_validator.transcript(file_location)
+                os.remove(file_location)
+                return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
+            # Delete the file after processing
+            
+        except Exception as e:
+            return {"status": "FAILED", "data": str(e)}
 
+    else:
+        return RedirectResponse(url='/login')
+    
