9eec662995e786fc9994adc192eed2aa45ba6463
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-07-02 12:37:06 +0530

    change in get user function

--- src/dependency.py
+++ src/dependency.py
@@ -2,8 +2,12 @@ from starlette.requests import Request
 from fastapi import HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED
 
+# def get_current_user(request: Request):
+#     user = request.session.get('user')
+#     if not user:
+#         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+#     return user
+
 def get_current_user(request: Request):
-    user = request.session.get('user')
-    if not user:
-        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized")
-    return user
+   return  request.session.get('user')
+    
