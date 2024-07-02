fc589695436e8075278a7a700ccddff31740ad23
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-07-02 14:26:17 +0530

    added user management

--- src/endpoints/user_management.py
+++ src/endpoints/user_management.py
@@ -8,19 +8,27 @@ from src.dependency import get_current_user
 router = APIRouter()
 
 class AddUser(BaseModel):
-    user_name: str
     email: str
     password: str
     first_name: str
     last_name: str
     phone_number: str
+    role : str
 
 class Login(BaseModel):
-    user_name : int
     email: str
     password: str
 
+class EditUser(BaseModel):
+    email: str
+    password: str
+    role : str
+    first_name: str
+    last_name: str
 
+class DeleteUser(BaseModel):
+    email: str
+   
 
 # @router.post("/user_login")
 # async def add_user(Log: Login):
@@ -35,12 +43,12 @@ class Login(BaseModel):
 @router.post("/add_user")
 async def add_user(Add: AddUser ):
     if get_current_user() ==1:
-        value = mf_user_management.add_user(username=Add.user_name,
-                                            email=Add.email,
+        value = mf_user_management.add_user(email=Add.email,
                                             password=Add.password,
                                             first_name=Add.first_name,
                                             last_name=Add.last_name,
                                             phone_number=Add.phone_number,
+                                            role = Add.role
                                             )
         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
     else:
@@ -58,4 +66,28 @@ async def list_user():
     else:
         return RedirectResponse(url='/login')
     
+    
+@router.post("/edit_user")
+async def edit_user(Edit: EditUser ):
+    if get_current_user() ==1:
+        value = mf_user_management.edit_user(email=Edit.email,
+                                            password=Edit.password,
+                                            role = Edit.role,
+                                            first_name= Edit.first_name ,
+                                            last_name=Edit.last_name
+                                            )
+        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
+    else:
+        return RedirectResponse(url='/login')
+    
+@router.get("/delete_user")
+async def delete_user(Delete: DeleteUser):
+    if get_current_user() ==1:
+        value = mf_user_management.delete_user(Delete.email)
+        if value ==1:
+            {"status": "SUCCESS", "data": "User deleted successfully"}
+        else:
+            {"status": "FAILED", "data": value}
+    else:
+        return RedirectResponse(url='/login')
     
