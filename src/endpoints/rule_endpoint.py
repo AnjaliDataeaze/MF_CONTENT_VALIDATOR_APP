b5b11152c6f8f9e7e48f9c98dd2fac9cb17e0ff4
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-07-03 14:46:16 +0530

    removed user authentication

--- src/endpoints/rule_endpoint.py
+++ src/endpoints/rule_endpoint.py
@@ -28,44 +28,86 @@ class ListRulesByProgram(BaseModel):
 
 
 
+# @router.get("/list_rules")
+# def list_rules():
+#     if get_current_user == 1:
+#         value, data = mf_validator.list_rules()
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+# @router.post("/list_rules_by_program")
+# def list_rules_by_program(rule: ListRulesByProgram):
+#     if get_current_user ==1:
+#         value, data = mf_validator.list_rules_by_program(rule.program_id)
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+# @router.post("/add_rule")
+# async def add_rule(rule: AddRule):
+#     if get_current_user ==1:
+#         value = mf_validator.add_rule(rule.rulename, rule.media_type, rule.description, rule.program_type, rule.disclaimer)
+#         return {"status": "SUCCESS" if value == 1 
+#                 else "FAILED", "data": "Rule added successfully !!!" if value == 1 else value}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+
+# @router.post("/edit_rule")
+# async def edit_rule(rule: EditRule):
+#     if get_current_user ==1:
+#         value = mf_validator.edit_rule(rule.rule_id, rule.rulename, rule.description, rule.disclaimer)
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule updated successfully !!!" if value == 1 else value}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+# @router.delete("/delete_rule")
+# def delete_rule(rule : DeleteRule):
+#     if get_current_user ==1:
+#         value = mf_validator.delete_rule(rule.rule_id)
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule deleted successfully !!!" if value == 1 else value}
+#     else:
+#         return RedirectResponse(url='/login')
+    
 @router.get("/list_rules")
 def list_rules():
-    if get_current_user() == 1:
-        value, data = mf_validator.list_rules()
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user == 1:
+    value, data = mf_validator.list_rules()
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
+    # else:
+    #     return RedirectResponse(url='/login')
     
 @router.post("/list_rules_by_program")
 def list_rules_by_program(rule: ListRulesByProgram):
-    if get_current_user() ==1:
-        value, data = mf_validator.list_rules_by_program(rule.program_id)
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user ==1:
+    value, data = mf_validator.list_rules_by_program(rule.program_id)
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
+    # else:
+    #     return RedirectResponse(url='/login')
     
 @router.post("/add_rule")
 async def add_rule(rule: AddRule):
-    if get_current_user() ==1:
-        value = mf_validator.add_rule(rule.rulename, rule.media_type, rule.description, rule.program_type, rule.disclaimer)
-        return {"status": "SUCCESS" if value == 1 
-                else "FAILED", "data": "Rule added successfully !!!" if value == 1 else value}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user ==1:
+    value = mf_validator.add_rule(rule.rulename, rule.media_type, rule.description, rule.program_type, rule.disclaimer)
+    return {"status": "SUCCESS" if value == 1 
+            else "FAILED", "data": "Rule added successfully !!!" if value == 1 else value}
+    # else:
+    #     return RedirectResponse(url='/login')
     
 
 @router.post("/edit_rule")
 async def edit_rule(rule: EditRule):
-    if get_current_user() ==1:
-        value = mf_validator.edit_rule(rule.rule_id, rule.rulename, rule.description, rule.disclaimer)
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule updated successfully !!!" if value == 1 else value}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user ==1:
+    value = mf_validator.edit_rule(rule.rule_id, rule.rulename, rule.description, rule.disclaimer)
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule updated successfully !!!" if value == 1 else value}
+    # else:
+    #     return RedirectResponse(url='/login')
     
 @router.delete("/delete_rule")
 def delete_rule(rule : DeleteRule):
-    if get_current_user() ==1:
-        value = mf_validator.delete_rule(rule.rule_id)
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule deleted successfully !!!" if value == 1 else value}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user ==1:
+    value = mf_validator.delete_rule(rule.rule_id)
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Rule deleted successfully !!!" if value == 1 else value}
+    # else:
+    #     return RedirectResponse(url='/login')
--- src/endpoints/user_management.py
+++ src/endpoints/user_management.py
@@ -10,24 +10,27 @@ router = APIRouter()
 class AddUser(BaseModel):
     email: str
     password: str
-    first_name: str
-    last_name: str
-    phone_number: str
+    firstName: str
+    lastName: str
+    phoneNumber: str
     role : str
+    status: str
 
 class Login(BaseModel):
     email: str
     password: str
 
 class EditUser(BaseModel):
-    email: str
-    password: str
-    role : str
     first_name: str
     last_name: str
+    email: str
+    phone_number: str
+    role : str
+    status: str
+    
 
 class DeleteUser(BaseModel):
-    email: str
+    user_id: int
    
 
 # @router.post("/user_login")
@@ -40,54 +43,112 @@ class DeleteUser(BaseModel):
 #     else:
 #         return {"status": "NOT_FOUND", "data": "User Not Found"}
 
-@router.post("/add_user")
-async def add_user(Add: AddUser ):
-    if get_current_user() ==1:
-        value = mf_user_management.add_user(email=Add.email,
-                                            password=Add.password,
-                                            first_name=Add.first_name,
-                                            last_name=Add.last_name,
-                                            phone_number=Add.phone_number,
-                                            role = Add.role
-                                            )
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
-    else:
-        return RedirectResponse(url='/login')
+# @router.post("/add_user")
+# async def add_user(Add: AddUser ):
+#     if get_current_user ==1:
+#         value = mf_user_management.add_user(email=Add.email,
+#                                             password=Add.password,
+#                                             first_name=Add.first_name,
+#                                             last_name=Add.last_name,
+#                                             phone_number=Add.phone_number,
+#                                             role = Add.role
+#                                             )
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
+#     else:
+#         return RedirectResponse(url='/login')
     
   
-@router.get("/list_user")
+# @router.get("/list_user")
+# async def list_user():
+#     if get_current_user ==1:
+#         value, data = mf_user_management.list_user()
+#         if value ==1:
+#             {"status": "SUCCESS", "data": data}
+#         else:
+#             {"status": "FAILED", "data": data}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+    
+# @router.post("/edit_user")
+# async def edit_user(Edit: EditUser ):
+#     if get_current_user ==1:
+#         value = mf_user_management.edit_user(email=Edit.email,
+#                                             password=Edit.password,
+#                                             role = Edit.role,
+#                                             first_name= Edit.first_name ,
+#                                             last_name=Edit.last_name
+#                                             )
+#         return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+# @router.get("/delete_user")
+# async def delete_user(Delete: DeleteUser):
+#     if get_current_user ==1:
+#         value = mf_user_management.delete_user(Delete.email)
+#         if value ==1:
+#             {"status": "SUCCESS", "data": "User deleted successfully"}
+#         else:
+#             {"status": "FAILED", "data": value}
+#     else:
+#         return RedirectResponse(url='/login')
+    
+
+
+@router.post("/add_user")
+async def add_user(Add: AddUser ):
+    value = mf_user_management.add_user(email=Add.email,
+                                        password=Add.password,
+                                        first_name=Add.firstName,
+                                        last_name=Add.lastName,
+                                        phone_number=Add.phoneNumber,
+                                        role = Add.role,
+                                        status = Add.status
+                                        )
+    
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
+    # else:
+    #     return RedirectResponse(url='/login')
+    
+  
+@router.get("/list_users")
 async def list_user():
-    if get_current_user() ==1:
-        value, data = mf_user_management.list_user()
-        if value ==1:
-            {"status": "SUCCESS", "data": data}
-        else:
-            {"status": "FAILED", "data": data}
+    # if get_current_user ==1:
+    value, data = mf_user_management.list_user()
+    print("Value-->",value)
+    print("Data", data)
+    if value == 1:
+        return {"status": "SUCCESS", "data": data}
     else:
-        return RedirectResponse(url='/login')
+        return {"status": "FAILED", "data": data}
+    # else:
+    #     return RedirectResponse(url='/login')
     
     
 @router.post("/edit_user")
 async def edit_user(Edit: EditUser ):
-    if get_current_user() ==1:
-        value = mf_user_management.edit_user(email=Edit.email,
-                                            password=Edit.password,
-                                            role = Edit.role,
-                                            first_name= Edit.first_name ,
-                                            last_name=Edit.last_name
-                                            )
-        return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
-    else:
-        return RedirectResponse(url='/login')
+    # if get_current_user ==1:
+    value = mf_user_management.edit_user(first_name= Edit.first_name ,
+                                         last_name=Edit.last_name,
+                                         email=Edit.email,
+                                         phone_number = Edit.phone_number,
+                                         role = Edit.role,
+                                         status= Edit.status
+                                        )
+    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User updated successfully !!!" if value == 1 else value}
+    # else:
+    #     return RedirectResponse(url='/login')
     
-@router.get("/delete_user")
+@router.delete("/delete_user")
 async def delete_user(Delete: DeleteUser):
-    if get_current_user() ==1:
-        value = mf_user_management.delete_user(Delete.email)
-        if value ==1:
-            {"status": "SUCCESS", "data": "User deleted successfully"}
-        else:
-            {"status": "FAILED", "data": value}
+    # if get_current_user ==1:
+    value = mf_user_management.delete_user(Delete.user_id)
+
+    if value ==1:
+        return {"status": "SUCCESS", "data": "User deleted successfully"}
     else:
-        return RedirectResponse(url='/login')
+        return {"status": "FAILED", "data": value}
+    # else:
+    #     return RedirectResponse(url='/login')
     
