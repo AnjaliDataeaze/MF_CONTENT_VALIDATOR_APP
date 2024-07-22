4734e15b5d3068b89ef0c2cf96cbd5f6316eb45b
Ubuntu <ubuntu@ip-172-31-42-144.ap-south-1.compute.internal>
2024-07-22 06:34:05 +0000

    change in responses

--- src/endpoints/program.py
+++ src/endpoints/program.py
@@ -10,7 +10,8 @@ class AddProgram(BaseModel):
     name: str
     description: str
     rules : List[str]
-                
+    created_by : str
+
 class DeleteProgram(BaseModel):
     program_id: int
 
@@ -22,26 +23,23 @@ class EditProgram(BaseModel):
 
 
 
-
 @router.get("/list_programs")
 def list_programs():
-    value, data = mf_validator.list_programs()
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": data}
-    
-    
+    return mf_validator.list_programs() 
+
+
 @router.post("/add_program")
 async def add_program(program: AddProgram):
-    value = mf_validator.add_program(program.name, program.description, program.rules)
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Program added successfully !!!" if value == 1 else value}
-    
-    
+    return mf_validator.add_program(program.name, program.description, program.rules, program.created_by)
+
+
 @router.post("/edit_program")
 async def edit_program(program: EditProgram):
-    value = mf_validator.edit_program(program.program_id, program.name, program.description, program.rules)
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Program edited successfully !!!" if value == 1 else value}
-    
+    return mf_validator.edit_program(program.program_id, program.name, program.description, program.rules)
+
+
 @router.delete("/delete_program")
 def delete_program(program: DeleteProgram):
-    value = mf_validator.delete_program(program.program_id)
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "Program deleted successfully !!!" if value == 1 else value}
-    
+    return mf_validator.delete_program(program.program_id)
+
+
--- src/endpoints/user_management.py
+++ src/endpoints/user_management.py
@@ -33,49 +33,40 @@ class DeleteUser(BaseModel):
 
 @router.post("/add_user")
 async def add_user(Add: AddUser ):
-    value = mf_user_management.add_user(email=Add.email,
-                                        password=Add.password,
-                                        first_name=Add.firstName,
-                                        last_name=Add.lastName,
-                                        phone_number=Add.phoneNumber,
-                                        role = Add.role,
-                                        status = Add.status
-                                        )
+    return mf_user_management.add_user(
+            email=Add.email, 
+            password=Add.password, 
+            first_name=Add.firstName, 
+            last_name=Add.lastName, 
+            phone_number=Add.phoneNumber, 
+            role = Add.role, 
+            status = Add.status
+            )
     
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User Added successfully !!!" if value == 1 else value}
-    
-  
 @router.get("/list_users")
-async def list_user():
-    value, data = mf_user_management.list_user()
-    if value == 1:
-        return {"status": "SUCCESS", "data": data}
-    else:
-        return {"status": "FAILED", "data": data}
-   
+async def list_users():
+    return mf_user_management.list_users()
+
+
+
 @router.get("/filter_user")
 async def filter_user(search: str):
-    result = mf_user_management.filter_user(search)
-    return result
+    return mf_user_management.filter_user(search)
 
-
+ 
 @router.post("/edit_user")
-async def edit_user(Edit: EditUser ):
-    value = mf_user_management.edit_user(first_name= Edit.firstName ,
-                                         last_name=Edit.lastName,
-                                         email=Edit.email,
-                                         phone_number = Edit.phoneNumber,
-                                         role = Edit.role,
-                                         status= Edit.status
-                                        )
-    return {"status": "SUCCESS" if value == 1 else "FAILED", "data": "User updated successfully !!!" if value == 1 else value}
-    
-    
+async def edit_user(edit: EditUser):
+    return mf_user_management.edit_user(
+        first_name=edit.firstName,
+        last_name=edit.lastName,
+        email=edit.email,
+        phone_number=edit.phoneNumber,
+        role=edit.role,
+        status=edit.status
+    )
+
+
 @router.delete("/delete_user")
 async def delete_user(Delete: DeleteUser):
-    value = mf_user_management.delete_user(Delete.user_id)
-    if value ==1:
-        return {"status": "SUCCESS", "data": "User deleted successfully"}
-    else:
-        return {"status": "FAILED", "data": value}
-    
+    return mf_user_management.delete_user(Delete.user_id)
+
