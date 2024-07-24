9aaf54d93a46e4f76f2be21e929e14d4b625185e
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-07-24 18:36:11 +0530

    flow change in validation

--- src/endpoints/validation.py
+++ src/endpoints/validation.py
@@ -33,62 +33,62 @@ async def validation(file: UploadFile = File(...), program_type: str = Form(...)
         return {"status": "FAILED", "data": str(e)}
 
     
-@router.post("/video_validation")
-async def validation(file: UploadFile = File(...), program_type: str = Form(...), operation: List[str] = Form(...)):
-    try:
-        file_location = f"temp_files/{file.filename}"
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-
-        if len(operation)==2:
-            response = mf_validator.frame_analysis(file_location,program_type)
-            data = mf_validator.transcript(file_location, program_type)
-            os.remove(file_location)
-            return {"statu":"SUCCESS","DATA":[{"operation": "frame" , "data": response}, {"operation": "audio" , "data":data}]}
-        else:
-            if operation[0] == 'frame_analysis':
-                response = mf_validator.frame_analysis(file_location,program_type)
-                os.remove(file_location)
-                return {"status": "SUCCESS" , "data": response}
-            
-            elif operation[0] == 'audio_analysis':
-                data = mf_validator.transcript(file_location, program_type)
-                os.remove(file_location)
-                return {"status":"Success", "Data":data}
-                
-    except Exception as e:
-        return {"status": "FAILED", "data": str(e)}
-
 # @router.post("/video_validation")
-# async def validation(file: UploadFile = File(...), program_type: str = Form(...), operation:str = Form(...)):
+# async def validation(file: UploadFile = File(...), program_type: str = Form(...), operation: List[str] = Form(...)):
 #     try:
-#         operations = json.loads(operation)
-#         print("operations-->", operations)
-#         print(type(operations))
 #         file_location = f"temp_files/{file.filename}"
 #         os.makedirs(os.path.dirname(file_location), exist_ok=True)
 #         with open(file_location, "wb") as buffer:
 #             shutil.copyfileobj(file.file, buffer)
 
-#         if len(operations)==2:
+#         if len(operation)==2:
 #             response = mf_validator.frame_analysis(file_location,program_type)
-#             data1, data2 = mf_validator.transcript(file_location, program_type)
+#             data = mf_validator.transcript(file_location, program_type)
 #             os.remove(file_location)
-#             return {{"status": "frame" , "data": response}, {"status": "audio" , "Data": {"Data1": data1, "Data2":data2}}}
+#             return {"statu":"SUCCESS","DATA":[{"operation": "frame" , "data": response}, {"operation": "audio" , "data":data}]}
 #         else:
-#             if operations[0] == 'frame_analysis':
+#             if operation[0] == 'frame_analysis':
 #                 response = mf_validator.frame_analysis(file_location,program_type)
 #                 os.remove(file_location)
 #                 return {"status": "SUCCESS" , "data": response}
-
-#             elif operations[0] == 'audio_analysis':
-#                 print("calling Audio analysis")
-#                 data1, data2 = mf_validator.transcript(file_location, program_type)
+            
+#             elif operation[0] == 'audio_analysis':
+#                 data = mf_validator.transcript(file_location, program_type)
 #                 os.remove(file_location)
-#                 return {"Data1":data1,"Data2": data2}
+#                 return {"status":"Success", "Data":data}
+                
 #     except Exception as e:
 #         return {"status": "FAILED", "data": str(e)}
+
+@router.post("/video_validation")
+async def validation(file: UploadFile = File(...), program_type: str = Form(...), operation:str = Form(...)):
+    try:
+        operations = json.loads(operation)
+        print("operations-->", operations)
+        print(type(operations))
+        file_location = f"temp_files/{file.filename}"
+        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        if len(operations)==2:
+            response = mf_validator.frame_analysis(file_location,program_type)
+            data1, data2 = mf_validator.transcript(file_location, program_type)
+            os.remove(file_location)
+            return {{"status": "frame" , "data": response}, {"status": "audio" , "Data": {"Data1": data1, "Data2":data2}}}
+        else:
+            if operations[0] == 'frame_analysis':
+                response = mf_validator.frame_analysis(file_location,program_type)
+                os.remove(file_location)
+                return {"status": "SUCCESS" , "data": response}
+
+            elif operations[0] == 'audio_analysis':
+                print("calling Audio analysis")
+                data1, data2 = mf_validator.transcript(file_location, program_type)
+                os.remove(file_location)
+                return {"Data1":data1,"Data2": data2}
+    except Exception as e:
+        return {"status": "FAILED", "data": str(e)}
 
 
 
